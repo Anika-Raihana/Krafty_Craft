9b4745d5bd3c9f6a2eda91b157a881d944f5bee3
Anika-Raihana <76659091+Anika-Raihana@users.noreply.github.com>
2022-09-04 23:07:39 +0600

    second commit

--- App_Login/tests.py
+++ App_Login/tests.py
@@ -1,3 +1,78 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from App_Login.models import Profile, User, MyUserManager
+from App_Login.forms import UserProfileForm,SignUpForm
+
 
 # Create your tests here.
+
+
+
+#Testing for models classes
+class MyUserManagerTests(TestCase):
+    def test_create_user(self):
+        user = User.objects._create_user('rayhananika234@gmail.com', '1234abcd')
+        self.assertTrue(isinstance(user, User))
+
+    def test_create_superuser(self):
+        user = User.objects.create_superuser('is_staff', 'is_superuser')
+        self.assertTrue(isinstance(user, User))
+
+
+class UserTests(TestCase):
+    def setUp(self):
+        self.user = User(email = "rayhananika234@gmail.com")
+
+    def test__str__(self):
+        self.assertEqual(self.user.__str__(), "rayhananika234@gmail.com")
+
+    def test_get_full_name(self):
+        self.assertEqual(self.user.get_full_name(), "rayhananika234@gmail.com")
+
+    def test_get_short_name(self):
+        self.assertEqual(self.user.get_short_name(), "rayhananika234@gmail.com")
+
+
+class ProfileTests(TestCase):
+    def setUp(self):
+        self.profile = Profile(username="Anika", address_1="Mymensingh",zipcode="4587",city="Mymensingh",country="Bangladesh")
+
+    def test__str__(self):
+        self.assertEqual(self.profile.__str__(), "Anika's Profile")
+
+    def test_is_fully_filled(self):
+        self.assertIs(self.profile.is_fully_filled(),False)
+
+
+
+
+
+#Testing for forms classes
+class ProfileFormTests(TestCase):
+    def test_meta_class(self):
+        form = UserProfileForm(data={})
+        self.assertTrue(form.is_valid())
+
+
+class SignUpFormTests(TestCase):
+    def test_meta_class(self):
+        form = SignUpForm(data={})
+        self.assertFalse(form.is_valid())
+
+
+class ViewsTest(TestCase):
+    def test_sign_up(self):
+        response = self.client.get(reverse('App_Login:signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_login(self):
+        response = self.client.get(reverse('App_Login:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_logout(self):
+        response = self.client.get(reverse('App_Login:logout'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_profile(self):
+        response = self.client.get(reverse('App_Login:profile'))
+        self.assertEqual(response.status_code, 302)
--- App_Payment/views.py
+++ App_Payment/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.urls import reverse
 from django.contrib import messages
-#models and forms
+
 from App_Order.models import Order, Cart
 from App_Payment.forms import BillingAddress
 from App_Payment.forms import BillingForm
 
-
 from django.contrib.auth.decorators import login_required
 
-
-
-# Create your views here.
 @login_required
 def checkout(request):
     saved_address = BillingAddress.objects.get_or_create(user=request.user)
@@ -28,9 +23,7 @@ def checkout(request):
             form = BillingForm(instance=saved_address)
             messages.success(request, f"Shipping Address Saved!")
     order_qs = Order.objects.filter(user=request.user, ordered=False)
-    #print(order_qs)
     order_items = order_qs[0].orderitems.all()
-    #print(order_items)
     order_total = order_qs[0].get_totals()
     return render(request, 'App_Payment/checkout.html', context={"form":form, "order_items":order_items, "order_total":order_total, "saved_address":saved_address})
 
@@ -47,27 +40,17 @@ def payment(request):
         messages.info(request, f"Please complete profile details!")
         return redirect("App_Login:profile")
 
-
-
     status_url = request.build_absolute_uri(reverse("App_Shop:home"))
-    #print(status_url)
-    #mypayment.set_urls(success_url=status_url, fail_url=status_url, cancel_url=status_url, ipn_url=status_url)
-
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     order_items = order_qs[0].orderitems.all()
     order_items_count = order_qs[0].orderitems.count()
     order_total = order_qs[0].get_totals()
 
-    #mypayment.set_product_integration(total_amount=Decimal(order_total), currency='BDT', product_category='Mixed', product_name=order_items, num_of_item=order_items_count, shipping_method='Courier', product_profile='None')
+    mypayment.set_product_integration(total_amount=Decimal(order_total), currency='BDT', product_category='Mixed', product_name=order_items, num_of_item=order_items_count, shipping_method='Courier', product_profile='None')
+    current_user = request.user
+    mypayment.set_customer_info(name=current_user.profile.full_name, email=current_user.email, address1=current_user.profile.address_1, address2=current_user.profile.address_1, city=current_user.profile.city, postcode=current_user.profile.zipcode, country=current_user.profile.country, phone=current_user.profile.phone)
 
-
-    #current_user = request.user
-    #mypayment.set_customer_info(name=current_user.profile.full_name, email=current_user.email, address1=current_user.profile.address_1, address2=current_user.profile.address_1, city=current_user.profile.city, postcode=current_user.profile.zipcode, country=current_user.profile.country, phone=current_user.profile.phone)
-
-    #mypayment.set_shipping_info(shipping_to=current_user.profile.full_name, address=saved_address.address, city=saved_address.city, postcode=saved_address.zipcode, country=saved_address.country)
-
-    #response_data = mypayment.init_payment()
-    #return redirect(response_data['GatewayPageURL'])
+    mypayment.set_shipping_info(shipping_to=current_user.profile.full_name, address=saved_address.address, city=saved_address.city, postcode=saved_address.zipcode, country=saved_address.country)
 
 
 @login_required
